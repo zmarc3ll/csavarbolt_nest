ec9cef1ab7789549b079cefe80ce47da859b1a2f
zmarc3ll <zubermarcellpetrik@gmail.com>
2023-01-31 10:06:26 +0100

    get, post, delete added and tested

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post, Render } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Render } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { AppService } from './app.service';
+import CsavarData from './CsavarData';
 
 @Controller()
 export class AppController {
@@ -14,4 +15,30 @@ export class AppController {
   index() {
     return { message: 'Welcome to the homepage' };
   }
+
+  @Get('csavar')
+  async csavarList() {
+    const csavarbolt = this.dataSource.getRepository(CsavarData);
+    return await csavarbolt.find();
+  }
+
+  @Post('csavar')
+  createCsavar(@Body() csavar: CsavarData) {
+    csavar.id = undefined;
+    const csavarbolt = this.dataSource.getRepository(CsavarData);
+    csavarbolt.save(csavar);
+  }
+
+  @Delete('csavar/:id')
+  deleteCsavar(@Param('id') id: number) {
+    const csavarbolt = this.dataSource.getRepository(CsavarData);
+    csavarbolt.delete(id);
+  }
+
+  @Post('csavar/:id/rendeles')
+  orderCsavar(@Param('id') id: number) {
+    const csavarbolt = this.dataSource.getRepository(CsavarData);
+
+  }
+
 }
